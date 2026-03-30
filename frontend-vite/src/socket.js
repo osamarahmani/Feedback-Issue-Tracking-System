2f430c3c7bd95b2a44e0b6fa686bb99cd767a7c1
Lakshithadevi <lakshithadevi2020@gmail.com>
2026-03-30 23:34:39 +0530

    socket

--- frontend-vite/src/socket.js
+++ frontend-vite/src/socket.js
@@ -1,16 +1,19 @@
 io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
   socket.on("join-issue", (issueId) => {
     socket.join(issueId);
+    console.log(`User joined issue ${issueId}`);
   });
 
   socket.on("send-message", ({ issueId, message }) => {
     io.to(issueId).emit("receive-message", message);
   });
 
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
 });
-
-import { io } from "socket.io-client";
 
 export const socket = io("https://feedback-issue-tracking-system.onrender.com", {
   transports: ["websocket"]
